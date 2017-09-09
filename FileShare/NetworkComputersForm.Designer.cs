cba0425cb223064405b9ccd2241364318902fcc8
Dion <dionvanderlinden@live.nl>
2017-09-09 13:45:27 +0200

    Icoon veranderd + DPI settings + network form font DPI

--- FileShare/NetworkComputersForm.Designer.cs
+++ FileShare/NetworkComputersForm.Designer.cs
@@ -93,8 +93,7 @@
             // 
             // NetworkComputersForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(184, 311);
             this.ControlBox = false;
             this.Controls.Add(this.pb_RefreshAnimation);
--- FileShare/Properties/Resources.Designer.cs
+++ FileShare/Properties/Resources.Designer.cs
@@ -101,6 +101,16 @@ namespace FileShare.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon FileShareIcon256 {
+            get {
+                object obj = ResourceManager.GetObject("FileShareIcon256", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Refresh_Animation_46 {
